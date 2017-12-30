ee421788ef9b78a96b2e16e908b5b13200e3d2d7
jakk <jakk@jakks-iMac.local>
2017-12-30 22:26:36 +0700

    refactor

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -14,23 +14,35 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate ();
 	}
 
-    private void ProcessInput() {
-        if (Input.GetKey(KeyCode.Space)) {
-            rigidBody.AddRelativeForce(Vector3.up);
-            if (!audioSource.isPlaying) {
-                audioSource.Play();
-            }
-        } else {
-            audioSource.Stop();
-        }
-        if (Input.GetKey(KeyCode.A)) {
+    private void Rotate ()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
             transform.Rotate(Vector3.forward);
         }
-        if (Input.GetKey(KeyCode.D)) {
+        if (Input.GetKey(KeyCode.D))
+        {
             transform.Rotate(-Vector3.forward);
+        }
+    }
+
+    private void Thrust ()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 }
